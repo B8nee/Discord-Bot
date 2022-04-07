91950b47040bc15a57d80b7fe5b19b60c6b936ad
B8nee <danyradut2017@gmail.com>
2022-04-07 09:35:15 +0200

    app created

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+Create an .env file and insert this:
+
+DISCORD_TOKEN={your-bot-token}
+DISCORD_GUILD={your-guild-token}
---
+++ bot.py
@@ -0,0 +1,37 @@
+import os
+import discord
+import random
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} si e\' connesso al server Cazzate Time!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    b8ne_quotes = [
+        'ABABABABABA',
+        'UGU GAGA UGU GAGA',
+        'Coglione',
+        'Fallito',
+        'Ucciditi'
+    ]
+    
+    if message.content == 'scimmia':
+        response = random.choice(b8ne_quotes)
+        await message.channel.send(response)
+        
+bot.activity = discord.Game(name='Demon Slayer')
+    
+    
+bot.run(TOKEN)
